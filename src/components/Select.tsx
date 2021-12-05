c6803d6396bd4632fa9d71d816677d7c128a3adc
Jing Yi Zhao <jingyi@pm61data.com>
2021-12-05 11:02:27 -0800

    init

---
+++ src/components/Select.tsx
@@ -0,0 +1,8 @@
+import type { ReactElement } from 'react';
+
+interface IProp {}
+const Select = (): ReactElement => {
+    return <div>GGWP asdfdasf adsfdasfads asdfadsfas</div>;
+};
+
+export default Select;
